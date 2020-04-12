40fa1958d9fb107ed6fdaf75ed8b8a4cbbfb2f28
unknown <belendd99@gmail.com>
2020-04-12 20:01:29 +0200

    dasd

---
+++ public/script_old.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function fetchMessage() {
+  //  var xmlHttp = new XMLHttpRequest();
+    //xmlHttp.open("GET", "/hello", false);
+    //xmlHttp.send(null);
+	document.body.style.backgroundImage = "url('https://thumbs.gfycat.com/GloriousFlimsyHedgehog-size_restricted.gif')";
+    document.getElementById("message").innerHTML= "";
+	document.getElementById("textScreenshot").setAttribute("src", "https://thumbs.gfycat.com/GloriousFlimsyHedgehog-size_restricted.gif");
+}
---
+++ src/MyContentTable.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<main>
+	<table>
+		<tr>
+			<td>hola</td>
+		</tr>
+	</table>
+</main>
+
+<style>
+	table{
+		border: 1px solid black;
+	}
+</style>
